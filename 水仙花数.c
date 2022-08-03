9b2fa3e924081472eed748c5357d1ccceb0ddea9
ellery2 <93391541+ellery2@users.noreply.github.com>
2022-08-03 16:45:46 +0800

    Delete 水仙花数.c

--- 水仙花数.c
+++
@@ -1,4 +0,0 @@
-﻿已启动生成...
-1>------ 已启动生成: 项目: Project2, 配置: Debug x64 ------
-1>Project2.vcxproj -> C:\Users\Ellery\Desktop\code\库\test_demo\Project2\x64\Debug\Project2.exe
-========== “生成”: 1 成功，0 失败，0 更新，0 已跳过 ==========
