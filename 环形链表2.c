2394091b14ef3a848943181ca0abe7feee69ab39
Ellery <1711282380@qq.com>
2022-11-15 14:32:50 +0800

    快慢指针，从头和相遇点出发在入口相遇

--- 环形链表2.c
+++ 环形链表2.c
@@ -1,2 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
+struct ListNode* detectCycle(struct ListNode* head) {
+	struct ListNode* fast, * slow, *p = NULL, *phead = NULL;
+	fast = slow = head;
+	if ((fast == NULL) || (fast->next == NULL))
+		return false;
+	while (fast)
+	{
+		if (fast->next)
+			fast = fast->next->next;
+		else
+			return false;
+		slow = slow->next;
+		if (slow == fast)
+		{
+			p = slow;
+			break;
+		}
+	}
+	while (1)
+	{
+		p = p->next;
+		phead = phead->next;
+		if (phead == p)
+			return p;
+	}
+	if(p == NULL)
+		return false;
+}
