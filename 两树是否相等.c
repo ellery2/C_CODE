b93418945a148533afd006b2df5465e2a4dd2794
Ellery <1711282380@qq.com>
2022-12-28 14:58:10 +0800

    ||用得很好

---
+++ 两树是否相等.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if (p == NULL && q == NULL)
+    {
+        return true;
+    }
+    if (p == NULL || q == NULL)
+    {
+        return false;
+    }
+    if (p->val != q->val)
+    {
+        return false;
+    }
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
